313efc54460215797e2abd5626e22946d506a71e
Aaron Holder <af.holder01@gmail.com>
2019-08-07 20:02:43 -0500

    schema and seeds

---
+++ db/employee_seeds.sql
@@ -0,0 +1,22 @@
+USE eht_db;
+
+INSERT INTO employees(employee_name)
+VALUES("Aaron");
+
+INSERT INTO employees(employee_name)
+VALUES("Simon");
+
+INSERT INTO employees(employee_name)
+VALUES("Marlon");
+
+INSERT INTO employees(employee_name)
+VALUES("John");
+
+INSERT INTO employees(employee_name)
+VALUES("Kahuna");
+
+
+
+
+
+
