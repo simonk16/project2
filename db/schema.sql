4445b19843476d033edf8371724d28f77bab3184
Kraus, Simon <simon.kraus@aol.com>
2019-08-08 20:26:19 -0500

    edited schema

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,8 @@ USE eht_db;
 CREATE TABLE employees(
 id INT NOT NULL auto_increment,
 employeeName VARCHAR(255) not null,
+createdAt timestamp,
+updatedAt timestamp,
 PRIMARY KEY(id)
 );
 
@@ -14,6 +16,8 @@ CREATE TABLE projects(
 project_id INT NOT NULL auto_increment,
 project_title VARCHAR(255),
 employee_id int, 
+createdAt timestamp,
+updatedAt timestamp,
 PRIMARY KEY(project_id),
 FOREIGN KEY(employee_id) REFERENCES employees(id)
 );
