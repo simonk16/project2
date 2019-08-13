44b6875b297f3cdfea1ecde673246430f5785bdc
Kraus, Simon <simon.kraus@aol.com>
2019-08-13 15:55:04 -0500

    in the process of switching from ejs to handlebars, just wanna save my code...

--- public/js/action_page.js
+++ public/js/action_page.js
@@ -2,6 +2,8 @@ module.exports = function(app) {
   app.post("/public/js/action_page.js", function(req, res) {
     var $formData = req.body.selectedAction;
     console.log($formData);
-    res.send("You chose: " + $formData);
+    if ($formData === "logNewProject") {
+      
+    }
   });
 };
