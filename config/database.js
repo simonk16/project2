2b81d11f6aed7557d2cd99529e6a8accfb1ef0f6
Kraus, Simon <simon.kraus@aol.com>
2019-08-12 19:34:46 -0500

    passport

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+  connection: {
+    host: "localhost",
+    user: "root",
+    password: "Burbank!123"
+  },
+  database: "userDB",
+  users_table: "users"
+};
--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 require("dotenv").config();
+var session = require("express-session");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var morgan = require("morgan");
+
+var passport = require("passport");
+var flash = require("connect-flash");
 var express = require("express");
 var exphbs = require("express-handlebars");
 
@@ -6,6 +13,39 @@ var db = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+require("./config/passport")(passport); // pass passport for configuration
+app.use(morgan("dev")); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.set(
+    "Cache-Control",
+    "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
+  );
+  next();
+});
+app.set("view engine", "ejs"); // set up ejs for templating
+
+// required for passport
+app.use(
+  session({
+    secret: "some-secret",
+    resave: true,
+    saveUninitialized: true
+  })
+); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require("./routes/routes.js")(app, passport); // load our routes and pass in our app and fully configured passport
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
