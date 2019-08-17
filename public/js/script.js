3b754b25c90b81c0f561d45c83023658660007ee
Kraus, Simon <simon.kraus@aol.com>
2019-08-17 10:07:47 -0500

    cleaned up merge conflicts

--- public/js/script.js
+++ public/js/script.js
@@ -6,17 +6,16 @@ $("#submitButton").on("click", function(event) {
   } else if ($("input[value='logExistingProject']:checked").val()) {
     $("#myExistingModal").modal("toggle");
     $.get("/api/projects", function(response) {
-      console.log("res is ........."+response);
+      console.log("res is ........." + response);
       var projectsArr = response;
       for (var i = 0; i < projectsArr.length; i++) {
-        console.log(i)
+        console.log(i);
         var projectsOption = projectsArr[i].projectName;
         $("select").append("<li>" + projectsOption + "</li>");
       }
     });
   }
 });
-<<<<<<< HEAD
 
 $("#updateButton").on("click", function(event) {
   event.preventDefault();
@@ -25,58 +24,62 @@ $("#updateButton").on("click", function(event) {
   var hoursWorked = $("#hoursWorked").val();
   console.log(hoursWorked);
 });
-=======
 $.get("/api/data", function(data) {
-  
-    
-var svgWidth = 500, svgHeight = 300, radius = Math.min(svgWidth, svgHeight) / 2;
-var svg = d3.select('svg')
+  var svgWidth = 500,
+    svgHeight = 300,
+    radius = Math.min(svgWidth, svgHeight) / 2;
+  var svg = d3
+    .select("svg")
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
-
-var g = svg.append("g")
+  var g = svg
+    .append("g")
     .attr("transform", "translate(" + radius + "," + radius + ")");
 
-var color = d3.scaleOrdinal(d3.schemeCategory10);
+  var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-var pie = d3.pie().value(function(d) {
+  var pie = d3.pie().value(function(d) {
     return d.hourName;
-});
+  });
 
-var path = d3.arc()
+  var path = d3
+    .arc()
     .outerRadius(radius)
     .innerRadius(0);
 
-var arc = g.selectAll("arc")
+  var arc = g
+    .selectAll("arc")
     .data(pie(data))
     .enter()
     .append("g");
 
-arc.append("path")
+  arc
+    .append("path")
     .attr("d", path)
-    .transition().delay(function(d,i) {
-        return i * 500; }).duration(500)
-    .attr("fill", function(d) { 
-        return color(d.data.hourName);
-     });
-
-var label = d3.arc()
-     .outerRadius(radius)
-     .innerRadius(0);
-
-arc.append("text")
-     .attr("transform", function(d) {
-         return "translate(" + label.centroid(d) + ")";
-     })
-     .attr("text-anchor", "middle")
-     .transition()
-	 .delay(500)
-     .text(function(d) {
-         return d.data.hourName + ":" + d.data.projectName + "%";
-     });
-
-
+    .transition()
+    .delay(function(d, i) {
+      return i * 500;
     })
-  
->>>>>>> master
+    .duration(500)
+    .attr("fill", function(d) {
+      return color(d.data.hourName);
+    });
+
+  var label = d3
+    .arc()
+    .outerRadius(radius)
+    .innerRadius(0);
+
+  arc
+    .append("text")
+    .attr("transform", function(d) {
+      return "translate(" + label.centroid(d) + ")";
+    })
+    .attr("text-anchor", "middle")
+    .transition()
+    .delay(500)
+    .text(function(d) {
+      return d.data.hourName + ":" + d.data.projectName + "%";
+    });
+});
